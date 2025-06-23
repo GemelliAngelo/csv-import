1758f45b9d4cf43cb892e2c01bfe403b36758428
GemelliAngelo <gemeangelo27@gmail.com>
2025-06-23 15:54:32 +0200

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function App() {
   const [file, setFile] = useState();
-  const [array, setArray] = useState([]);
+  const [items, setItems] = useState([]);
 
   const fileReader = new FileReader();
 
@@ -14,7 +14,7 @@ function App() {
     const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
     const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
 
-    const newArray = csvRows.map((row) => {
+    const array = csvRows.map((row) => {
       const values = row.split(",");
       const obj = csvHeader.reduce((object, header, index) => {
         object[header] = values[index];
@@ -23,7 +23,7 @@ function App() {
       return obj;
     });
 
-    setArray(newArray);
+    setItems(array);
   };
 
   const handleOnSubmit = (e) => {
@@ -39,40 +39,45 @@ function App() {
     }
   };
 
-  const headerKeys = Object.keys(Object.assign({}, ...array));
+  const headerKeys = Object.keys(Object.assign({}, ...items));
 
   return (
     <>
-      <form>
-        <input
-          type="file"
-          accept=".csv"
-          name="test"
-          id="test"
-          onChange={handleOnChange}
-        />
-        <button
-          onClick={(e) => {
-            handleOnSubmit(e);
-          }}
-        >
-          Stampa CSV in una tabella
-        </button>
+      <h1 className="text-center">CSV Input</h1>
+      <form className="py-5">
+        <div className="input-group w-50 mx-auto">
+          <input
+            type="file"
+            accept=".csv"
+            name="file"
+            id="file"
+            className="form-control"
+            onChange={handleOnChange}
+          />
+          <button
+            className="btn btn-dark"
+            onClick={(e) => {
+              handleOnSubmit(e);
+            }}
+          >
+            Stampa CSV in una tabella
+          </button>
+        </div>
       </form>
 
-      <table className="table">
+      <table className="table table-striped">
         <thead>
           <tr>
             {headerKeys.map((key) => (
               <th scope="col" key={key}>
-                {key}
+                {key.toUpperCase()}
               </th>
             ))}
           </tr>
         </thead>
 
         <tbody>
-          {array.map((item, index) => (
+          {items.map((item, index) => (
             <tr key={index}>
               {Object.values(item).map((val) => (
                 <td key={val}>{val}</td>
