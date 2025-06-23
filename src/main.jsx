d29e9b6ffd714e923ee6e2646e4abe3f2dd02435
GemelliAngelo <gemeangelo27@gmail.com>
2025-06-23 13:13:45 +0200

    import bootstrap + fix scaffolding

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
+
+import "./assets/css/index.css";
 
 createRoot(document.getElementById("root")).render(<App />);
